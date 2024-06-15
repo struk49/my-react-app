ea12a64500d24e7396eb4a472b410633bf13e5c1
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-15 07:47:51 +0000

    learning about setState and how to use setState

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+
+function App() {
+  return (
+    <div className="App">
+      <StatefulGreetingWithPrevState />
+    </div>
+  );
+}
+
+export default App;
